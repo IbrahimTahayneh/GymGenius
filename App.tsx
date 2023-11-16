7b1148c72284e24932a3f15376ef9b4169559ca3
Ibrahim Tahayna <ibrahimt@aui.io>
2023-11-16 09:40:29 +0200

    fix: loading for fonts

--- App.tsx
+++ App.tsx
@@ -1,17 +1,35 @@
 import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { StyleSheet, Text, View } from "react-native";
-import loadFonts from "./src/theme/FontLoader";
-import { useEffect } from "react";
+import { useCallback } from "react";
+import { FONT_FAMILIES } from "./src/theme";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  useEffect(() => {
-    // Load fonts when the app starts
-    loadFonts();
-  }, []);
+  const [fontsLoaded] = useFonts({
+    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Light": require("./assets/fonts/Poppins-Light.ttf"),
+    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  const handleOnLayout = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync(); //hide the splashscreen
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.container} onLayout={handleOnLayout}>
+      <Text style={styles.titl}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -23,5 +41,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  titl: {
+    fontFamily: FONT_FAMILIES.POPPINS_REGULAR,
   },
 });
